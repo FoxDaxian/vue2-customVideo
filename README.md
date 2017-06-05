da4d9f0b8c6fef91cf00399994ec1322ac20e0b7
a13821190779 <945039036@qq.com>
2017-06-05 21:07:40 +0800

    how to split css?

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-<h2 align="center">vue2-custom-video<h2>
+#vue2-video
+轻量级vue2 video组件
 
-[Live Demo](https://a13821190779.github.io/vue-video/)
+```
+第一步，(c)npm下载
+(c)npm i vue2-video -S
+```
+
+```javascript
+//第二步，main.js引入
+import VueVideo from 'vue2-video'
+Vue.use(VueVideo);
+```
+
+```javascript
+//第三步，使用
+<vue-video src='xxx.mp4'></vue-video>
+```
+
+
+更多详见[文档](https://a13821190779.github.io/vue-video/)
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,100 @@
+<style scoped lang="scss">
+	.video{
+		padding: 24px;
+		background-color: rgba(#5B311B, 0.8);
+		.tempWrap{
+			width: 800px;
+			margin:0 auto;
+			h2{
+				span{
+					font-size: 30px;
+				}
+				color:#fff;
+			}
+		}
+	}
+	.video:nth-of-type(2n){
+		background-color: rgba(#1D3156, 0.8);
+	}
+	.video:first-child{
+		margin-bottom: 8px;
+		background-color: #16A085;
+	}
+</style>
 <template>
 	<div id="app">
-		<!-- <video-com
-		src="http://devtest.qiniudn.com/%E8%8B%A5%E8%83%BD%E7%BB%BD%E6%94%BE%E5%85%89%E8%8A%92.mp4"
-		width="800px"
-		height="500px"
-		:playImg='require("../../../static/play.png")'
-		:poster='require("../../../static/eva.jpg")'
-		:menu="true"
-		></video-com> -->
-		<vue-video
-		src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
-		:definition="definition"
-		:playImg='require("@/assets/play.png")'
-		:poster='require("@/assets/eva.jpg")'
-		:menu="true"
-		></vue-video>
-		<pre><code ref="code">{{size_code}}</code></pre>
+		<div class="video">
+			<div class="tempWrap">
+				<h2><span>第一步：</span>安装并绑定组件</h2>
+				<pre><code ref="code">{{intro}}</code></pre>
+			</div>
+		</div>		<div class="video">
+		<div class="tempWrap">
+			<h2><span>形态1：</span>仅仅一个src属性，具备基础的功能</h2>
+			<vue-video
+			src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+			></vue-video>
+			<pre><code ref="code1">{{video1}}</code></pre>
+		</div>
 	</div>
+	<div class="video">
+		<div class="tempWrap">
+			<h2><span>形态2：</span>自定义宽高(默认为800*500)</h2>
+			<vue-video
+			src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+			width="800px"
+			height="600px"
+			></vue-video>
+			<pre><code ref="code2">{{video2}}</code></pre>
+		</div>
+	</div>
+	<div class="video">
+		<div class="tempWrap">
+			<h2><span>形态3：</span>指定暂停播放显示图片和poster</h2>
+			<vue-video
+			src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+			:playImg='require("@/assets/play.png")'
+			:poster='require("@/assets/eva.jpg")'
+			></vue-video>
+			<pre><code ref="code3">{{video3}}</code></pre>
+		</div>
+	</div>
+	<div class="video">
+		<div class="tempWrap">
+			<h2><span>形态4：</span>自定义右键菜单</h2>
+			<vue-video
+			src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+			:menu="true"
+			></vue-video>
+			<pre><code ref="code4">{{video4}}</code></pre>
+		</div>
+	</div>
+	<div class="video">
+		<div class="tempWrap">
+			<h2><span>形态5：</span>切换清晰度</h2>
+			<video-com
+			src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+			:definition="definition"
+			></video-com>
+			<pre><code ref="code5">{{video5}}</code></pre>
+		</div>
+	</div>
+	<div class="video">
+		<div class="tempWrap">
+			<h2><span>究极形态：</span>合体进化</h2>
+			<video-com
+			src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+			width="800px"
+			height="600px"
+			:playImg='require("@/assets/play.png")'
+			:poster='require("@/assets/eva.jpg")'
+			:menu="true"
+			:definition="definition"
+			></video-com>
+			<pre><code ref="code6">{{video6}}</code></pre>
+		</div>
+	</div>
+</div>
 </template>
 
 <script>
@@ -31,14 +109,64 @@
 					low:"http://oqvlh6ipq.bkt.clouddn.com/eva_low.mp4",
 					high:"http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
 				},
-				size_code: `
+				intro:`
+				//首先 (c)npm i vue2-video -S
+
+				import VueVideo from '../lib/'
+
+				Vue.use(VueVideo)
+				`,
+				video1: `
 				<vue-video
 				src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+				></vue-video>
+				`,
+				video2: `
+				<vue-video
+				src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+				width="800px"
+				height="600px"
+				></vue-video>
+				`,
+				video3: `
+				// @为path.resolve(__dirname,'../src/')的别名(alias)
+				<vue-video
+				src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+				:playImg='require("@/assets/play.png")'
+				:poster='require("@/assets/eva.jpg")'
+				></vue-video>
+				`,
+				video4: `
+				<vue-video
+				src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+				:menu="true"
+				></vue-video>
+				`,
+				video5: `
+				<video-com
+				src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
 				:definition="definition"
+				></video-com>
+				
+				data(){
+					return {
+						definition:{
+							low:"http://oqvlh6ipq.bkt.clouddn.com/eva_low.mp4",
+							high:"http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+						}
+					}
+				}
+				`,
+				video6: `
+				<video-com
+				src="http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
+				width="800px"
+				height="600px"
 				:playImg='require("@/assets/play.png")'
 				:poster='require("@/assets/eva.jpg")'
 				:menu="true"
-				></vue-video>
+				:definition="definition"
+				></video-com>
 
 				data(){
 					return {
@@ -47,18 +175,17 @@
 							high:"http://oqvlh6ipq.bkt.clouddn.com/eva_normal.mp4"
 						}
 					}
-				},
-				`,
+				}
+				`
 			}
 		},
 		components: {
 			VideoCom
 		},
 		mounted(){
-			hljs.highlightBlock(this.$refs.code)
+			Object.values(this.$refs).forEach(function( el, i ) {
+				hljs.highlightBlock(el)
+			});
 		}
 	}
 </script>
-
-<style scoped lang="scss">
-</style>
